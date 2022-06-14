125277ed4c0f0ddf160dc531fc795861b01736ce
abbieoliverr <abioli23@bergen.org>
2022-06-14 04:07:33 -0400

    sudoku

---
+++ distraction/scripts/sudoku.js
@@ -0,0 +1,96 @@
+var numSelected = null;
+var tileSelected = null;
+
+var errors = 0;
+
+var board = [
+    "-3--8-451",
+    "---31--7-",
+    "7---4563-",
+    "--4----63",
+    "---9-458-",
+    "3-9---12-",
+    "-68----9-",
+    "--74--81-",
+    "---82-7-5"
+]
+var solution = [
+    "932687451",
+    "645319278",
+    "781245639",
+    "874152963",
+    "216934587",
+    "359768124",
+    "468571392",
+    "527493816",
+    "193826745"
+]
+
+window.onload = function () {
+    setGame();
+}
+
+function setGame() {
+    for (let i = 1; i <= 9; i++) {
+        let number = document.createElement("div");
+        number.id = i;
+        number.innerText = i;
+        number.addEventListener("click", selectNumber);
+        number.classList.add("number");
+        document.getElementById("digits").appendChild(number);
+    }
+
+    for (let r = 0; r < 9; r++) {
+        for (let c = 0; c < 9; c++) {
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            if (board[r][c] != "-") {
+                tile.innerText = board[r][c];
+                tile.classList.add("tile-start");
+            }
+
+            if (r == 2 || r == 5) {
+                tile.classList.add("horizontal-line");
+            }
+            
+            if (c == 2 || c == 5) {
+                tile.classList.add("vertical-line");
+            }
+
+            tile.addEventListener("click", selectTile);
+            tile.classList.add("tile");
+            document.getElementById("board").append(tile);
+        }
+    }
+}
+
+function selectNumber() {
+    if (numSelected != null) {
+        numSelected.classList.remove("number-selected");
+    }
+    numSelected = this;
+    numSelected.classList.add("number-selected");
+}
+
+function selectTile() {
+    if (numSelected) {
+        if (this.innerText != "") {
+            return;
+        }
+
+
+        let coords = this.id.split("-");
+        let r = parseInt(coords[0]);
+        let c = parseInt(coords[1]);
+
+        if (solution[r][c] == numSelected.id) {
+            this.innerText = numSelected.id;
+        }
+        else {
+            errors += 1;
+            document.getElementById("errors").innerText = errors;
+        }
+    }
+}ß
+
+
